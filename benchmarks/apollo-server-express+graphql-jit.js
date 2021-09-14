021c12b0199f614a519fa0a98ad8952204f46325
Mehmet Yüksel Polat <mehmet@cofeapp.com>
2021-09-15 02:24:12 +0300

    Added apollo-server-express variants

---
+++ benchmarks/apollo-server-express+graphql-jit.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const { ApolloServer } = require('apollo-server-express')
+const express = require('express')
+const { createApolloSchema } = require('../lib/schemas/createApolloSchema')
+const { parse } = require('graphql')
+const { compileQuery } = require('graphql-jit')
+
+const cache = {}
+
+const app = express()
+const schema = createApolloSchema()
+const server = new ApolloServer({
+  schema,
+  executor: ({ source, context }) => {
+    if (!(source in cache)) {
+      const document = parse(source)
+      cache[source] = compileQuery(schema, document)
+    }
+
+    return cache[source].query({}, context, {})
+  }
+})
+server.applyMiddleware({ app })
+app.listen(4001)
